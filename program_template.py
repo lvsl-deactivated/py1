3ef79b729fc7e65c3b10ee4bad10a696e0b3e1eb
lvsl <chin@shaftsoft.ru>
2011-02-12 09:42:51 +0500

    Added function template

---
+++ program_template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#
+# После объявления "coding: utf-8"
+# можно писать комментарии на русском
+#
+
+# шаблон программы
+
+def main(args):
+    ''' Ваш код '''
+    pass
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv[:]))
+
