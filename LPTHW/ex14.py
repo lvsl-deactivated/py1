84690324f3b694d3b4b84aafb7532bf25428a5bd
lvsl <chin@shaftsoft.ru>
2011-02-26 00:56:14 +0500

    Added ex14.py

--- LPTHW/ex14.py
+++ LPTHW/ex14.py
@@ -1,1 +1,23 @@
 # coding: utf-8
+
+from sys import argv
+
+script_name, user = argv
+prompt = '> '
+
+print "Привет %s! Я %s" % (user, script_name)
+print "Вы любите программировать?"
+like_it = raw_input(prompt)
+
+print "Где вы живёте?"
+lives = raw_input(prompt)
+
+print "Какой у вас компьютер?"
+computer = raw_input(prompt)
+
+print """
+Итак %r, вы отвечаете %r.
+Я знаю, где вы живете: %r
+и какой у вас компьютер: %r.
+\t -- как мило!
+""" % (user, like_it, lives, computer)
