e7f72f0d25119e9115bbef880e976d89382226d3
lvsl <chin@shaftsoft.ru>
2011-02-25 10:11:58 +0500

    Added ex10.py

--- LPTHW/ex10.py
+++ LPTHW/ex10.py
@@ -1,1 +1,21 @@
 # coding: utf-8
+
+# Escape символы
+
+print "I am not 6'2\" tall"
+print 'I am not 6\'2" tall'
+
+tabby_cat = "\t I'm tabbed in"
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat"
+fat_cat = """
+i'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
