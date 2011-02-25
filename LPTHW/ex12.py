1fe2720dbedfe7baa22a4efd45a9a8f706c7dec1
lvsl <chin@shaftsoft.ru>
2011-02-26 00:22:20 +0500

    Added ex12.py

--- LPTHW/ex12.py
+++ LPTHW/ex12.py
@@ -1,1 +1,18 @@
 # coding: utf-8
+
+fname = raw_input("Ваше имя? ")
+mname = raw_input("Ваше отчество? ")
+lname = raw_input("Ваша фимилия? ")
+
+print  "Здравствуйте, %r %r %r!" % (
+    fname,
+    mname,
+    lname,
+)
+
+# Упражения:
+#
+# 1. Найдите в документации описание raw_input
+#
+# 2. Найти также в документаци описание open
+#
