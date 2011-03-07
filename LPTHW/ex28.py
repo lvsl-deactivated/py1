28650af7b86a86465b93c261387b3500d57f71e0
lvsl <chin@shaftsoft.ru>
2011-03-07 21:01:16 +0500

    Added ex28.py

--- LPTHW/ex28.py
+++ LPTHW/ex28.py
@@ -1,1 +1,32 @@
 # coding: utf-8
+
+# Логика в python
+
+print True and True
+print False and True
+print 1 == 1 and 2 == 1
+print "test" == "test"
+print 1 == 1 or 2 != 1
+print True and 1 == 1
+print False and 0 != 0
+print True or 1 == 1
+print "test" == "Test"
+print not (True and False)
+print not (1 == 1 and 0 != 1)
+print not (10 == 1 or 1000 == 1000)
+print not (1 != 10 or 3 == 4)
+print not ("testing" == "testing" and "foo" == "bar")
+print 1 == 1 and not ("testing" == 1 or 1 == 0)
+print "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+print 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
+
+
+# Упражнения:
+#
+# 1. Напишите комментарий для каждой строки
+#
+# 2. Зачем в некоторых выражениях нужны скобки?
+#    Что будет если их убрать?
+#    Что будет если их поставить там где их не было?
+#    Что такое приоритет операций?
+#
