ac53e5584fb7995ad11da5f692808d6d6508796f
lvsl <chin@shaftsoft.ru>
2011-03-07 18:34:33 +0500

    Added ex25.py

--- LPTHW/ex25.py
+++ LPTHW/ex25.py
@@ -1,1 +1,23 @@
 # coding: utf-8
+
+
+# Подключение модулей
+import ex24
+# или
+from ex24 import counties
+
+print ex24.counties == counties
+
+# Взаимодействие черех интерфейс
+print ex24.get_country_by_index(143)
+# или напрямую
+print counties[143]
+
+
+# Упражнения:
+#
+# 1. Разберитемь с примером, напишите комментарий, над каждой строкой
+#
+# 2. Какие достоинства и недостатки использования интерфейсов
+#    к данным? (get_country_by_index, get_index_by_country)
+#
