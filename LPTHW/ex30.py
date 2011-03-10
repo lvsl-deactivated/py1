a6b01ce1844c67b93063e3d23d8cbe670da5ff39
lvsl <chin@shaftsoft.ru>
2011-03-10 23:45:08 +0500

    Added ex30.py

--- LPTHW/ex30.py
+++ LPTHW/ex30.py
@@ -1,1 +1,28 @@
 # coding: utf-8
+
+# Конструкция if/elif.../else
+
+def check(people, cats, dogs):
+    if people > cats and people > dogs:
+        result = "OK!"
+    elif people > cats:
+        result = "Собаки!"
+    elif people > dogs:
+        result = "Кошки!"
+    else:
+        result = "Кошки и Собаки!"
+
+    return result
+
+
+print check(10,20,30)
+print check(20,20,30)
+print check(30,20,30)
+print check(40,20,30)
+
+# Упражнения:
+#
+# 1. Перепишите функцию check() без использования elif.
+#    Как проще?
+#    Подсказка: Используйте вложенные if/else
+#
