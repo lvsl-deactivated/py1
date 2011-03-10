eeaf9874858ce1c59ac6592eabc6fad4ed2e3400
lvsl <chin@shaftsoft.ru>
2011-03-10 23:24:52 +0500

    Added ex29.py

--- LPTHW/ex29.py
+++ LPTHW/ex29.py
@@ -1,1 +1,25 @@
 # coding: utf-8
+
+# Конструкция if
+
+people = 10
+cats = 20
+dogs = 30
+
+if people < cats:
+    print "Кошки!"
+
+if people < dogs:
+    print "Собаки!"
+
+people += 30
+
+if people > cats and people > dogs:
+    print people, cats, dogs
+    print "OK!"
+
+# Упражнения:
+#
+# 1. Всегда ли необходимы отступы после if?
+#    Что будет если убрать отступы?
+#
