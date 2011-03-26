efc6b3dac6d844605008e786958fb7465de7c75c
lvsl <chin@shaftsoft.ru>
2011-03-26 11:14:46 +0500

    Added POST handler

--- lec7/s.py
+++ lec7/s.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-
+import cgi
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
+from cStringIO import StringIO
 
 from jinja_example2 import main as example_html
 
@@ -18,6 +19,40 @@ class H(BaseHTTPRequestHandler):
 
         self.wfile.write(example_html())
         self.wfile.write(self.path)
+        self.wfile.flush()
+        self.connection.close()
+
+
+    def do_POST(self):
+        content_type, pdict = cgi.parse_header(self.headers['content-type'])
+
+        max_chunk_size = 2048
+        size_remaining = int(self.headers["content-length"])
+
+        raw_data = ''
+        while size_remaining:
+            chunk_size = min(size_remaining, max_chunk_size)
+            data = self.rfile.read(chunk_size)
+            size_remaining -= len(data)
+            raw_data = raw_data + data
+
+        request_dict = {}
+        is_binary = False
+        if 'application/x-www-form-urlencoded' in content_type:
+            request_dict = cgi.parse_qs(raw_data)
+
+        elif 'multipart/form-data' in content_type:
+            request_dict = cgi.parse_multipart(StringIO(raw_data), pdict)
+            is_binary = True
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-Encoding', 'utf-8')
+        self.end_headers()
+
+        self.wfile.write(raw_data)
+        self.wfile.write(request_dict)
+        self.wfile.write(is_binary)
         self.wfile.flush()
         self.connection.close()
 
